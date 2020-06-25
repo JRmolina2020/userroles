bb1c86911242e687696308d00a8e4f7d0f32b6f3
JRMOLINA2020 <JRMOLINA2020@GMAIL.COM>
2020-06-25 16:36:44 -0500

    dsdsdsdsds

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Spatie\Permission\Models\Permission;
+use App\Model\Permission;
 
 class PermissionController extends Controller
 {
