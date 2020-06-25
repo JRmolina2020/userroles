c518b3e5a8c99ffe0d78a7133446a5d927236069
JRMOLINA2020 <JRMOLINA2020@GMAIL.COM>
2020-06-25 16:50:59 -0500

    dsdsdsdsdsa2

--- resources/js/components/permissions/index.vue
+++ resources/js/components/permissions/index.vue
@@ -1,14 +1,22 @@
 <template>
   <div>
-    <Add></Add>
+    <Add ref="form_role"></Add>
+    <List-Role v-on:show="show"></List-Role>
   </div>
 </template>
 <script>
+import ListRole from "./list";
 import Add from "./add";
 export default {
   name: "users",
   components: {
+    ListRole,
     Add
+  },
+  methods: {
+    show(row) {
+      this.$refs.form_role.show(row);
+    }
   }
 };
 </script>
