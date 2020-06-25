3ad0e0e05ffe1a8ff39703008c3e2589d1e17329
JRMOLINA2020 <JRMOLINA2020@GMAIL.COM>
2020-06-25 16:47:12 -0500

    dsdsdsdsds2

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use App\Model\Permission;
+use Spatie\Permission\Models\Permission;
 
 class PermissionController extends Controller
 {
--- resources/js/components/permissions/index.vue
+++ resources/js/components/permissions/index.vue
@@ -1,22 +1,14 @@
 <template>
   <div>
-    <Add ref="form_role"></Add>
-    <List-Role v-on:show="show"></List-Role>
+    <Add></Add>
   </div>
 </template>
 <script>
-import ListRole from "./list";
 import Add from "./add";
 export default {
   name: "users",
   components: {
-    ListRole,
     Add
-  },
-  methods: {
-    show(row) {
-      this.$refs.form_role.show(row);
-    }
   }
 };
 </script>
